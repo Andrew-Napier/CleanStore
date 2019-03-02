e5b1969b2577a3f22c300b18f97d43b7aace486d
Andrew Napier <andrew.napier@runbox.com>
2019-03-02 15:25:25 +1100

    Made field delegate pick next text field

--- CleanStore/Scenes/CreateOrder/CreateOrderViewController.swift
+++ CleanStore/Scenes/CreateOrder/CreateOrderViewController.swift
@@ -14,79 +14,114 @@ import UIKit
 
 protocol CreateOrderDisplayLogic: class
 {
-  func displaySomething(viewModel: CreateOrder.Something.ViewModel)
+    func displaySomething(viewModel: CreateOrder.Something.ViewModel)
 }
 
-class CreateOrderViewController: UITableViewController, CreateOrderDisplayLogic
+class CreateOrderViewController: UITableViewController,
+    CreateOrderDisplayLogic
 {
     @IBOutlet var textFields: [UITextField]!
+    
+    // MARK: Shipping Method
     @IBOutlet weak var shippingMethodTextField: UITextField!
+    @IBOutlet var shippingMethodPicker: UIPickerView!
+    
+    // MARK: Expiration Date
     @IBOutlet weak var expirationDateTextField: UITextField!
+    @IBOutlet var expirationDatePicker: UIDatePicker!
+    
     var interactor: CreateOrderBusinessLogic?
-  var router: (NSObjectProtocol & CreateOrderRoutingLogic & CreateOrderDataPassing)?
-
-  // MARK: Object lifecycle
-  
-  override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
-  {
-    super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
-    setup()
-  }
-  
-  required init?(coder aDecoder: NSCoder)
-  {
-    super.init(coder: aDecoder)
-    setup()
-  }
-  
-  // MARK: Setup
-  
-  private func setup()
-  {
-    let viewController = self
-    let interactor = CreateOrderInteractor()
-    let presenter = CreateOrderPresenter()
-    let router = CreateOrderRouter()
-    viewController.interactor = interactor
-    viewController.router = router
-    interactor.presenter = presenter
-    presenter.viewController = viewController
-    router.viewController = viewController
-    router.dataStore = interactor
-  }
-  
-  // MARK: Routing
-  
-  override func prepare(for segue: UIStoryboardSegue, sender: Any?)
-  {
-    if let scene = segue.identifier {
-      let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
-      if let router = router, router.responds(to: selector) {
-        router.perform(selector, with: segue)
-      }
+    var router: (NSObjectProtocol & CreateOrderRoutingLogic & CreateOrderDataPassing)?
+    
+    @IBAction func expirationDatePickerValueChanged(_ sender: Any) {
     }
-  }
-  
-  // MARK: View lifecycle
-  
-  override func viewDidLoad()
-  {
-    super.viewDidLoad()
-    doSomething()
-  }
-  
-  // MARK: Do something
-  
-  //@IBOutlet weak var nameTextField: UITextField!
-  
-  func doSomething()
-  {
-    let request = CreateOrder.Something.Request()
-    interactor?.doSomething(request: request)
-  }
-  
-  func displaySomething(viewModel: CreateOrder.Something.ViewModel)
-  {
-    //nameTextField.text = viewModel.name
-  }
+    
+    // MARK: Object lifecycle
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?)
+    {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+        setup()
+    }
+    
+    required init?(coder aDecoder: NSCoder)
+    {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    // MARK: Setup
+    
+    private func setup()
+    {
+        let viewController = self
+        let interactor = CreateOrderInteractor()
+        let presenter = CreateOrderPresenter()
+        let router = CreateOrderRouter()
+        viewController.interactor = interactor
+        viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        router.viewController = viewController
+        router.dataStore = interactor
+    }
+    
+    // MARK: Routing
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?)
+    {
+        if let scene = segue.identifier {
+            let selector = NSSelectorFromString("routeTo\(scene)WithSegue:")
+            if let router = router, router.responds(to: selector) {
+                router.perform(selector, with: segue)
+            }
+        }
+    }
+    
+    // MARK: View lifecycle
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        
+        doSomething()
+    }
+    
+    // MARK: Do something
+    
+    //@IBOutlet weak var nameTextField: UITextField!
+    
+    func doSomething()
+    {
+        let request = CreateOrder.Something.Request()
+        interactor?.doSomething(request: request)
+    }
+    
+    func displaySomething(viewModel: CreateOrder.Something.ViewModel)
+    {
+        //nameTextField.text = viewModel.name
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) {
+            for textField in self.textFields {
+                if textField.isDescendant(of: cell) {
+                    textField.becomeFirstResponder()
+                }
+            }
+        }
+    }
+}
+
+extension CreateOrderViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        if let index = self.textFields.index(of: textField) {
+            if index < self.textFields.count-1 {
+                let nextTextField = self.textFields[index+1]
+                nextTextField.becomeFirstResponder()
+            }
+        }
+        return true
+    }
 }
